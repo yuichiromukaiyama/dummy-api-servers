25a0aaf055083f5cbd137075a6a91893b73c142f
Mukaiyama(**094**) <103568894+yuichiromukaiyama@users.noreply.github.com>
2024-04-25 12:21:22 +0900

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# dummy api servers
+
+テスト用
---
+++ server.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Any
+
+app = FastAPI()
+
+# Sample weather data
+weather_data = {
+    "japan": {
+        "tokyo": "Sunny",
+    },
+    "canada": {
+        "ottawa": "Rainy",
+    },
+    "usa": {
+        "washington_dc": "Cloudy",
+    },
+    "china": {
+        "beijing": "Snowy",
+    },
+}
+
+# Sample todo data
+todo_data: dict[str, Any] = {}
+
+
+class Task(BaseModel):
+    task: str
+
+
+@app.get("/weather")
+def get_weather(region: str, town: str, date: int):
+    print("access", region, town, date)  # date は割愛
+
+    try:
+        return {"result": weather_data[region][town]}
+    except:
+        return {"result": "could not be get"}
+
+
+@app.get("/todo/{username}")
+def get_todo_list(username: str):
+    print(f"access: POST:/todo/{username}")
+    if username in todo_data:
+        return todo_data[username]
+    else:
+        return []
+
+
+@app.post("/todo/{username}")
+def add_task(username: str, task: Task):
+    print(f"access: POST:/todo/{username}", task)
+    try:
+        if username in todo_data:
+            todo_data[username].append(task.task)
+        else:
+            todo_data[username] = [task.task]
+        return {"result": "Task added successfully"}
+    except:
+        return {"result": "Task add failed"}
